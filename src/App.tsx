1c940507dbf6269c59dcf736bd6dcfd13799b7ad
gusaiani <gustavo@poe.ma>
2020-11-05 09:34:22 -0300

    Stub canvas functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from "react";
 
 function App() {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  const startDrawing = () => {};
+  const endDrawing = () => {};
+  const draw = () => {};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <canvas
+      onMouseDown={startDrawing}
+      onMouseUp={endDrawing}
+      onMouseOut={endDrawing}
+      onMouseMove={draw}
+      ref={canvasRef}
+    />
   );
 }
 
